129bc0456924024075d9d185cec35695cb381999
witgeneration <116431256+witgeneration@users.noreply.github.com>
2022-10-25 14:17:18 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    princess.setImage(img`
+    animation.runImageAnimation(
+    princess,
+    [img`
         . . . . . f f 4 4 f f . . . . . 
         . . . . f 5 4 5 5 4 5 f . . . . 
         . . . f e 3 3 3 3 3 3 e f . . . 
@@ -16,10 +18,66 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         . . e f b d b d b d b b f e . . 
         . . . f f 1 d 1 d 1 d f f . . . 
         . . . . . f f b b f f . . . . . 
-        `)
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b f . 
+        f b b b b b b b b b b b b c f . 
+        f f b b b b b b b b b b c f . . 
+        . f c c c f f f f f f f e c . . 
+        . . . f b b d b d d e 4 4 e . . 
+        . . . f f 1 1 d 1 d e e f . . . 
+        . . . . . f b b f f f . . . . . 
+        `,img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b c f 
+        f b b b b b b b b b b b b b b f 
+        . f c c b b b b b b b b c c f . 
+        . . e 4 c f f f f f f c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        . f b b b b b b b b b b b b c f 
+        . f c b b b b b b b b b b b b f 
+        . . f c b b b b b b b b b b f f 
+        . . c e f f f f f f f c c c f . 
+        . . e 4 4 e d d b d b b f . . . 
+        . . . f e e d 1 d 1 1 f f . . . 
+        . . . . . f f f b b f . . . . . 
+        `],
+    500,
+    false
+    )
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    princess.setImage(img`
+    animation.runImageAnimation(
+    princess,
+    [img`
         . . . f 4 4 f f f f . . . . . . 
         . . f 4 5 5 4 5 f b f f . . . . 
         . . f 5 5 5 5 4 e 3 3 b f . . . 
@@ -36,10 +94,32 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . f b d b d b e e b f . . . . 
         . . f f 1 d 1 d 1 d f f . . . . 
         . . . . f f b b f f . . . . . . 
-        `)
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . f 4 4 f f f f . . . . . . 
+        . . f 4 5 5 4 5 f b f f . . . . 
+        . . f 5 5 5 5 4 e 3 3 b f . . . 
+        . . f e 4 4 4 e 3 3 3 3 b f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . . f e e e f b f b b b b f f . 
+        . . . e 4 4 f 1 e b b b b b f . 
+        . . . f 4 4 4 4 f b b b b b f . 
+        . . . f d d d e 4 4 b b b f . . 
+        . . . f d d d e 4 4 f f f . . . 
+        . . f d d d b b e e b b f . . . 
+        . . f b d 1 d 1 d d b f . . . . 
+        . . . f f f b b f f f . . . . . 
+        `],
+    500,
+    false
+    )
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    princess.setImage(img`
+    animation.runImageAnimation(
+    princess,
+    [img`
         . . . . . . f f f f 4 4 f . . . 
         . . . . f f b f 5 4 5 5 4 f . . 
         . . . f b 3 3 e 4 5 5 5 5 f . . 
@@ -56,10 +136,32 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . f b e e b d b d b f . . 
         . . . . f f d 1 d 1 d 1 f f . . 
         . . . . . . f f b b f f . . . . 
-        `)
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f 4 4 f . . . 
+        . . . . f f b f 5 4 5 5 4 f . . 
+        . . . f b 3 3 e 4 5 5 5 5 f . . 
+        . . f b 3 3 3 3 e 4 4 4 e f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . f f b b b b f b f e e e f . . 
+        . f b b b b b e 1 f 4 4 e . . . 
+        . f b b b b b f 4 4 4 4 f . . . 
+        . . f b b b 4 4 e d d d f . . . 
+        . . . f f f 4 4 e d d d f . . . 
+        . . . f b b e e b b d d d f . . 
+        . . . . f b d d 1 d 1 d b f . . 
+        . . . . . f f f b b f f f . . . 
+        `],
+    500,
+    false
+    )
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    princess.setImage(img`
+    animation.runImageAnimation(
+    princess,
+    [img`
         . . . . . f f 4 4 f f . . . . . 
         . . . . f 5 4 5 5 4 5 f . . . . 
         . . . f e 4 5 5 5 5 4 e f . . . 
@@ -76,8 +178,73 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . e f b d b d b d b b f e . . 
         . . . f f 1 d 1 d 1 d f f . . . 
         . . . . . f f b b f f . . . . . 
-        `)
+        `,img`
+        . . . . . . . f f . . . . . . . 
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f b 3 f f e e e e f f 3 b f . 
+        f f b b f b f e e f b f b b f f 
+        f b b b e 1 f 4 4 f 1 e b b b f 
+        . f b b e e 4 4 4 4 4 f b b f . 
+        . . f 4 4 4 e d d d b f e f . . 
+        . . f e 4 4 e d d d d c 4 e . . 
+        . . . f e e d d b d b b f e . . 
+        . . . f f 1 d 1 d 1 1 f f . . . 
+        . . . . . f f f b b f . . . . . 
+        `,img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . f f . . . . . . . 
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f b 3 f f e e e e f f 3 b f . 
+        f f b b f b f e e f b f b b f f 
+        f b b b e 1 f 4 4 f 1 e b b b f 
+        . f b b f 4 4 4 4 4 e e b b f . 
+        . . f e f b d d d e 4 4 4 f . . 
+        . . e 4 c d d d d e 4 4 e f . . 
+        . . e f b b d b d d e e f . . . 
+        . . . f f 1 1 d 1 d 1 f f . . . 
+        . . . . . f b b f f f . . . . . 
+        `],
+    500,
+    false
+    )
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.zapped.play()
+    otherSprite.destroy()
+    count += 1
+    if (count == 4) {
+        game.splash("Your time: " + game.runtime() / 1000, "segundos")
+        game.reset()
+    }
+})
+let apple: Sprite = null
+let count = 0
 let princess: Sprite = null
 scene.setBackgroundColor(12)
 scene.setTileMap(assets.image`myImage`)
@@ -157,3 +324,26 @@ princess.setScale(0.8, ScaleAnchor.Middle)
 princess.setPosition(0, 40)
 controller.moveSprite(princess)
 scene.cameraFollowSprite(princess)
+count = 0
+for (let index = 0; index < 4; index++) {
+    apple = sprites.create(img`
+        . . . . . . . e c 7 . . . . . . 
+        . . . . e e e c 7 7 e e . . . . 
+        . . c e e e e c 7 e 2 2 e e . . 
+        . c e e e e e c 6 e e 2 2 2 e . 
+        . c e e e 2 e c c 2 4 5 4 2 e . 
+        c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+        . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+        . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+        . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+        . . . 2 2 e e 4 4 4 2 e e . . . 
+        . . . . . 2 2 e e e e . . . . . 
+        `, SpriteKind.Food)
+    apple.setScale(0.8, ScaleAnchor.Middle)
+    scene.placeOnRandomTile(apple, 5)
+}
